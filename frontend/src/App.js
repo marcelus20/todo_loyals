f08bac20a78612c01733b64f942711c279d6bb91
marcelus <marcelus20felipe@gmail.com>
2020-04-10 22:26:15 +0100

    dockerized frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
-import Dashboard from "./components/Dashboard/Dashboard";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Dashboard />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
